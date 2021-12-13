5f59440aab0bc66e808c40a52c1b0c771b07010b
nanhangl <58681238+nanhangl@users.noreply.github.com>
2021-12-13 10:14:08 +0800

    Update create_subset_dataset.py

--- create_subset_dataset.py
+++ create_subset_dataset.py
@@ -6,15 +6,20 @@ import random
 from shutil import copyfile
 
 selected = []
-os.chdir("image_directory")
-main_dataset = ""
-subset_dataset = ""
+os.chdir("img_dir")
+main_dataset = "."
+subset_dataset = "subset"
 ls = os.listdir(main_dataset)
-subset_dataset_size = 10
+subset_dataset_size = 100
 
 while len(selected) < subset_dataset_size:
     index = random.randint(0, len(ls) - 1)
-    if index not in selected:
+    withoutExt = ls[index].split(".")[:-1]
+    withoutExt = '.'.join(withoutExt)
+    ext = ls[index].split(".")[-1]
+    if index not in selected and ext == "png":
         copyfile(f"{main_dataset}/{ls[index]}",
                  f"{subset_dataset}/{ls[index]}")
+        copyfile(f"{main_dataset}/annotations/{withoutExt}.xml",
+                 f"{subset_dataset}/{withoutExt}.xml")
         selected.append(index)
